98d1f99c9aed0e6397fd82fe98616563c4ab0e12
iwdath <lazorgrechak@gmail.com>
2024-01-31 03:07:41 +0200

    feat(feedback): add feedback section
    This commit adds a new 'feedback' section to the site. This section
    displays customer reviews. Each review includes the customer's name,
    social links, and comments.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menuMobile = document.querySelector('.header__list');
 
-menuBtn.addEventListener('click', ()=> {
+menuBtn.addEventListener('click', () => {
   menuMobile.classList.toggle('menu--open');
+});
+
+
+const swiperOne = new Swiper('.feedback__slider', {
+  spaceBetween: 20,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    1000: {
+      spaceBetween: 30,
+    },
+  },
 });
